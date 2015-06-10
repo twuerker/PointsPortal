da5da58b3cbd6b8593b4d60d3202fc3df7646fe3
Taylor Wuerker <twuerke1@gmail.com>
2015-06-10 18:33:01 -0400

    fixed hello typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hell0
+  def hello
     render text: "hello, world!"
   end
 
